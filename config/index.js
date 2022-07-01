286604481396e8b471c6dfbd5963ee1feaf43be8
Luca Martinelli <martinelliluca98@gmail.com>
2022-07-01 20:28:59 +0200

    First commit

--- config/index.js
+++ config/index.js
@@ -5,12 +5,12 @@ module.exports = {
     nodeOpenDoor: process.env.NODE_OPEN_DOOR || "http://localhost:1880/door/open"
   },
   database: {
-    host: process.env.DB_HOST || "192.168.178.2",
+    host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "MArtine_lli9824",
+    password: process.env.DB_PASSWORD || "root",
     database: process.env.DB_NAME || "boschouse"
   },
   telegram: {
-    api: process.env.TG_BOT_API || "5586957472:AAEUG8FUGswx7RuZ57VaS6hx2ec3jc20YM8"
+    api: process.env.TG_BOT_API || ""
   }
 }
