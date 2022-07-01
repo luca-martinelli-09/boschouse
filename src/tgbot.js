5aa2aa8ba6a1cb6dbfab2ddac605f95bb381bb47
Luca Martinelli <martinelliluca98@gmail.com>
2022-07-01 21:03:46 +0200

    Remove callback query when opening the door

--- src/tgbot.js
+++ src/tgbot.js
@@ -95,6 +95,7 @@ bot.on('callback_query', function onCallbackQuery(q) {
     axios
       .get(config.server.nodeOpenDoor)
       .then(function () {
+        bot.editMessageReplyMarkup(null);
         bot.sendMessage(q.message.chat.id, "✅ Porta aperta");
       })
       .catch(error => {
